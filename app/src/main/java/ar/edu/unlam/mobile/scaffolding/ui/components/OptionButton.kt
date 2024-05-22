93a261062beccd5f7b98b10ae55c7a987a447f75
Miguel Zurita <jzurita@alumno.unlam.edu.ar>
2024-05-21 22:24:01 -0300

    #7: Juego Clásico: Veo 4 opciones de nombres de paises para seleccionar mi respuesta
    + se agrego 4 componentes boton en columna
    + se agrego la pantalla de juego clasico y establece como primera a mostrar en el activity
    + se agrega el logo en blanco de "Flagsmania"

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/OptionButton.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/OptionButton.kt
@@ -1,27 +1,47 @@
 package ar.edu.unlam.mobile.scaffolding.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun OptionButton(text: String) {
+fun OptionButton(
+    text: String,
+    backgroundColor: Color = Color.White,
+    labelColor: Color = Color.Black,
+) {
     Button(
         modifier = Modifier.fillMaxWidth(),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.White,
-            contentColor = Color.Black
-        ),
+        colors =
+            ButtonDefaults.buttonColors(
+                containerColor = backgroundColor,
+                contentColor = labelColor,
+            ),
         onClick = {},
-        elevation = ButtonDefaults.buttonElevation(defaultElevation = 12.dp)
-    )
-    { Text(text = text) }
+        elevation = ButtonDefaults.buttonElevation(defaultElevation = 12.dp),
+    ) { Text(text = text) }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OptionButtonPreview() {
+    OptionButton("Argentina")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SuccessOptionButtonPreview() {
+    OptionButton("Argentina", Color.Green, Color.Black)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FailedOptionButtonPreview() {
+    OptionButton("Argentina", Color.Red, Color.Black)
 }
