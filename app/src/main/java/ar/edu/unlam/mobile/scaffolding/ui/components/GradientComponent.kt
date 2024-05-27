d9066b0ad5bcb0e55af0ce0aaac7f91bdeb531d0
Miguel Zurita <jzurita@alumno.unlam.edu.ar>
2024-05-27 18:09:39 -0300

    -f ajustar segun el linter

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/GradientComponent.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/GradientComponent.kt
@@ -1,45 +1,46 @@
 package ar.edu.unlam.mobile.scaffolding.ui.components
 
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun GradientComponent(height : Int){
-   Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(height.dp)
-            .clip(
-                RoundedCornerShape(
-                    bottomStart = 80.dp,
-                    bottomEnd = 80.dp
+fun GradientComponent(height: Int) {
+    Box(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .height(height.dp)
+                .clip(
+                    RoundedCornerShape(
+                        bottomStart = 80.dp,
+                        bottomEnd = 80.dp,
+                    ),
                 )
-            )
-            .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(
-                        Color(0xffC4007A),
-                        Color(0xffE4A82B)
-                    )
-                )
-            )
-
+                .background(
+                    brush =
+                        Brush.verticalGradient(
+                            colors =
+                                listOf(
+                                    Color(0xffC4007A),
+                                    Color(0xffE4A82B),
+                                ),
+                        ),
+                ),
     )
 }
 
 @Preview
 @Composable
-fun PreviewGradientComponent(){
-   GradientComponent(300)
+fun PreviewGradientComponent() {
+    GradientComponent(300)
 }
