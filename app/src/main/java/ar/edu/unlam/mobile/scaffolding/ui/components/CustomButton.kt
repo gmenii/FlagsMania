6f4eaa3ad0c0cbe06d3d42ef6e5a18a1988d1336
Nicolás Cartellone <ncartellone@alumno.unlam.edu.ar>
2024-05-28 08:55:59 -0300

    + add fontweight bold to button text

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/CustomButton.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/CustomButton.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import ar.edu.unlam.mobile.scaffolding.R
 import ar.edu.unlam.mobile.scaffolding.ui.theme.AppFont
 
@@ -41,5 +42,5 @@ fun CustomButton(
             ),
         onClick = onClick,
         // elevation = ButtonDefaults.buttonElevation(defaultElevation = 12.dp)
-    ) { Text(text = text, fontFamily = AppFont.Quicksand) }
+    ) { Text(text = text, fontFamily = AppFont.Quicksand, fontWeight = FontWeight.Bold) }
 }
