e80b423947d2af704924cc44ed46f8eeff198fa3
Giuliana Menicucci <144448550+gmenii@users.noreply.github.com>
2025-06-12 17:48:14 -0300

    Update CountryApiService.kt
    Modificación por problemas con la API

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/network/CountryApiService.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/network/CountryApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 // DEFINIR LA INTERFACE DE LA API
 interface CountryApiService {
-    @GET("api/countries")
-    suspend fun getAllCountries(): List<CountryResponse>
+    @GET("all")
+    suspend fun getCountries(): List<CountryResponse>
 }
